e2659785c4d5e21293c7ee9722fb30956ef63f89
Vaibhav Kumar <vaibhav.kumar@diggrowth.com>
2026-08-15 23:56:56 +0530

    ci file added

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-result1 = add(5,4)
-result2 = subtract(5,4)
-print(result1,result2)
+result1 = add(5, 4)
+result2 = subtract(5, 4)
+print(result1, result2)
